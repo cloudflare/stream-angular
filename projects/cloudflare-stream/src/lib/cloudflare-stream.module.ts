6d5acf55a9484fa0ce3e8cb1c433c0769b3df06a
Kevin Kipp <kkipp@cloudflare.com>
2022-04-07 17:00:45 -0500

    Fix SSR by checking platform before accessing document

--- projects/cloudflare-stream/src/lib/cloudflare-stream.module.ts
+++ projects/cloudflare-stream/src/lib/cloudflare-stream.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CloudflareStreamComponent } from './cloudflare-stream.component';
-import { DocumentWrapper, getDocument } from './document-wrapper';
+import { DocumentWrapper, getDocumentWrapper } from './document-wrapper';
 
 @NgModule({
   declarations: [CloudflareStreamComponent],
@@ -8,7 +8,7 @@ import { DocumentWrapper, getDocument } from './document-wrapper';
   // and Angular will refuse to render this non-standard
   // element unless we use this.
   schemas: [NO_ERRORS_SCHEMA],
-  providers: [{ provide: DocumentWrapper, useFactory: getDocument }],
+  providers: [{ provide: DocumentWrapper, useFactory: getDocumentWrapper }],
   imports: [],
   exports: [CloudflareStreamComponent],
 })
